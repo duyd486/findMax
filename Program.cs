28b086ac216b104d001db161239b8baf30a70b89
duyd486 <duyd486@gmail.com>
2024-04-09 22:09:09 +0700

    add du an
    add du an

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap do dai mang: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] myArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            myArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int max = myArray[0];
+        for (int i = 0; i < myArray.Length; i++) {
+            if (myArray[i] > max) {
+                max = myArray[i];
+            }
+        }
+        Console.WriteLine("Gia tri lon nhat cua mang la: " + max);
+    }
+
+}
